7cb7a914d1b53481d0a71875977c3ef1c837cada
Damian Kłos <alojzy231@gmail.com>
2022-11-10 10:42:51 +0100

    feat: primitive types sizeof task

--- wdp/lab1/3_6.c
+++ wdp/lab1/3_6.c
@@ -5,16 +5,30 @@ int main()
   unsigned char typUnsignedChar;
   signed char typSignedChar;
   int typInt;
-  unsigned int typUnsignedChar;
-  signed int typSignedChar;
+  unsigned int typUnsignedInt;
+  signed int typSignedInt;
   short typShort;
   long typLong;
+  unsigned long typUnsignedLong;
   float typFloat;
   double typDouble;
   long double typLongDouble;
   _Bool typBool;
 
-  printf("nazwa typu | rozmiar");
+  printf("nazwa typu | rozmiar (w bajtach)\n");
+  printf("char | %zu\n", sizeof(typChar));
+  printf("unsigned char | %zu\n", sizeof(typUnsignedChar));
+  printf("signed char | %zu\n", sizeof(typSignedChar));
+  printf("int | %zu\n", sizeof(typInt));
+  printf("unsigned int | %zu\n", sizeof(typUnsignedInt));
+  printf("signed int | %zu\n", sizeof(typSignedInt));
+  printf("short | %zu\n", sizeof(typShort));
+  printf("long | %zu\n", sizeof(typLong));
+  printf("unsigned long | %zu\n", sizeof(typUnsignedLong));
+  printf("float | %zu\n", sizeof(typFloat));
+  printf("double | %zu\n", sizeof(typDouble));
+  printf("long double | %zu\n", sizeof(typLongDouble));
+  printf("bool | %zu\n", sizeof(typBool));
 
   getchar();
   return 0;
