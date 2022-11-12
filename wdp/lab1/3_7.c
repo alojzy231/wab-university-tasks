9406874f83b17f966ce53f2f343fe56203f82fc1
Damian Kłos <alojzy231@gmail.com>
2022-11-12 14:15:33 +0100

    feat: 3.7 task

---
+++ wdp/lab1/3_7.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    const double y = ((0.5 * pow(sin(0.45), 2)) + (2 * tan(sqrt(2)))) / ((log10(14)) + (2 * exp(4)));
+
+    printf("Wartość działania: %lf\n", y);
+
+    return 0;
+}
