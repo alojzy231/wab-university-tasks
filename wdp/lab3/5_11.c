0728730c0d502d0e33d5d84f43c65f71a872e87e
alojzy231 <alojzy231@gmail.com>
2022-12-15 23:03:34 +0100

    feat: decode

--- wdp/lab3/5_11.c
+++ wdp/lab3/5_11.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-void printEncryptedChar(char c, int minValue, int maxValue)
+void printEncryptedChar(char c, int key, int minValue, int maxValue)
 {
-    char newC = c + 1;
+    char newC = c + key;
 
     if (c >= minValue && c <= maxValue)
     {
@@ -32,9 +32,29 @@ int main()
         if (c == 32)
             printf(" ");
 
-        printEncryptedChar(c, 48, 57);  // numbers
-        printEncryptedChar(c, 65, 90);  // uppercase
-        printEncryptedChar(c, 97, 122); // lowercase
+        printEncryptedChar(c, key, 48, 57);  // numbers
+        printEncryptedChar(c, key, 65, 90);  // uppercase
+        printEncryptedChar(c, key, 97, 122); // lowercase
     }
+
+    printf("Wprowadz wartosc klucza: ");
+    scanf("%d", &key);
+    getchar();
+    printf("Zdanie jakie chcesz odkodowac: ");
+
+    while (1)
+    {
+        c = getchar();
+
+        if (c == 27)
+            break;
+        if (c == 32)
+            printf(" ");
+
+        printEncryptedChar(c, -key, 48, 57);  // numbers
+        printEncryptedChar(c, -key, 65, 90);  // uppercase
+        printEncryptedChar(c, -key, 97, 122); // lowercase
+    }
+
     return 0;
 }
