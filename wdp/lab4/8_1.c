79d016e2029f02bf06d3237645196ae4371b7c79
Damian Kłos <alojzy231@gmail.com>
2023-01-08 23:33:47 +0100

    style: added whitespaces

--- wdp/lab4/8_1.c
+++ wdp/lab4/8_1.c
@@ -22,6 +22,7 @@ int main()
     {
         printf("Wybierz (1: pokaz (display), 2: wstaw (push), 3: usun(pop)), 4: Koniec..: ");
         scanf("%d", &choice);
+
         switch (choice)
         {
         case 1:
@@ -30,8 +31,10 @@ int main()
         case 2:
             printf("podaj liczbe ktora mam wstawic :");
             scanf("%d", &item);
+
             printf(" wstawiam %d\n", item);
             push(stack, item);
+
             break;
         case 3:
             pop(stack);
@@ -56,10 +59,12 @@ void display(int stack[])
     else
     {
         printf("Zawartosc stosu: ");
+
         for (i = top; i >= 1; i--)
         {
             printf("%d ", stack[i]);
         }
+
         printf("\n");
     }
 }
@@ -82,12 +87,14 @@ int pop(int stack[])
     if (top == 0)
     {
         printf("Stos jest pusty. Nie można usunąć elementu.\n");
+
         return -1;
     }
     else
     {
         int popped = stack[top];
         top--;
+
         return popped;
     }
 }
